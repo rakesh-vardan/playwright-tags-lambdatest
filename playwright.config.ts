18258a5879245942ce3ea963d0a57f54157b26d6
Rakesh Vardan <rakeshlearnit@gmail.com>
2025-07-06 20:19:40 +0530

    Adds the lambdatest cloud grid configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,30 +1,81 @@
-import { defineConfig, devices } from '@playwright/test';
+import type { PlaywrightTestConfig } from "@playwright/test";
+import { devices } from "@playwright/test";
 
-export default defineConfig({
-  testDir: './tests',
-  fullyParallel: true,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+// Playwright config to run tests on LambdaTest platform and local
+const config: PlaywrightTestConfig = {
+  testDir: "tests",
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+  timeout: 300000,
   use: {
     baseURL: 'https://ecommerce-playground.lambdatest.io',
-    headless: false,
+    // headless: false,
     trace: 'on-first-retry',
   },
-
   projects: [
+    // -- LambdaTest Config --
+    // name in the format: browserName:browserVersion:platform@lambdatest
+    // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+    // Use additional configuration options provided by Playwright if required: https://playwright.dev/docs/api/class-testconfig
+    // {
+    //   name: "chrome:latest:MacOS Ventura@lambdatest",
+    //   use: {
+    //     viewport: { width: 1920, height: 1080 },
+    //   },
+    // },
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-      // grep: /@hello/,
+      name: "chrome:latest:Windows 11@lambdatest",
+      use: {
+        viewport: { width: 1280, height: 720 },
+      },
     },
     // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
+    //   name: "MicrosoftEdge:latest:MacOS Ventura@lambdatest",
+    //   use: {
+    //     ...devices["iPhone 12 Pro Max"],
+    //   },
     // },
-
     // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
+    //   name: "pw-firefox:latest:Windows 11@lambdatest",
+    //   use: {
+    //     viewport: { width: 1280, height: 720 },
+    //   },
+    // },
+    // {
+    //   name: "pw-webkit:latest:Windows 10@lambdatest",
+    //   use: {
+    //     viewport: { width: 1920, height: 1080 },
+    //   },
+    // },
+    // Config for running tests in local
+    // {
+    //   name: "chrome",
+    //   use: {
+    //     browserName: "chromium",
+    //     channel: "chrome",
+    //   },
+    // },
+    // {
+    //   name: "safari",
+    //   use: {
+    //     browserName: "webkit",
+    //     viewport: { width: 1200, height: 750 },
+    //   },
+    // },
+    // {
+    //   name: "firefox",
+    //   use: {
+    //     browserName: "firefox",
+    //     viewport: { width: 800, height: 600 },
+    //   },
+    // },
+    // // Test in mobile viewport.
+    // {
+    //   name: "chrome@pixel5",
+    //   use: {
+    //     ...devices['iPhone 12 Pro Max'],
+    //   }
     // },
   ],
-});
+};
+
+export default config;
